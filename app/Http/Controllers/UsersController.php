7a3a68e9a9e0697e489a714d5d2e13661802e303
zoe606 <rive99erlangga@gmail.com>
2018-12-27 16:27:20 +0700

    management users

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Yajra\Datatables\Datatables;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.users.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // menggambil semua data yang ada pada form 
+        // return $request->all();
+
+        // validasi
+        // memanggil method validate yang ada di controller
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+            'role' => 'required'
+        ]);
+        
+        // overriding nilai pwd 
+        $request['password'] = bcrypt($request->get('password'));
+        $request['avatar'] = $request->get('avatar') ? $request->get('avatar') : '/images/user-icon.png';
+
+        // save ke db 
+        User::create($request->all());
+
+        return redirect()->route('admin.users.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        // compact = data binding
+        return view('admin.users.show', compact('user'));
+        
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('admin.users.edit', compact('user'));        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // validasi
+        // memanggil method validate yang ada di controller
+        $this->validate($request, [
+            'name' => 'string|max:255',
+            'email' => 'string|email|max:255|unique:users,email,'. $id,
+            'password' => 'nullable|string|min:6',
+            'role' => 'required'
+        ]);
+
+        $user = User::findOrFail($id);
+        // validasi
+        $request['password'] = $request->get('password') ? bcrypt($request->get('password')) : $user->password;
+        $request['avatar'] = $request->get('avatar') ? $request->get('avatar') : '/images/user-icon.png';
+
+        $user->update($request->all());
+
+        return redirect()->route('admin.users.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if (!User::destroy($id)) return redirect()->back();
+        return redirect()->route('admin.users.index');
+    }
+
+    public function dataTable()
+    {
+        $users = User::query();
+        return DataTables::of($users)
+            ->addColumn('user', function($users) {
+                return '<img src="' . asset('images/user-icon.png') . '" height="32" width="32">'.
+                $users->name;
+            })
+            ->addColumn('action', function($users) {
+                return view('layouts.admin.partials._action', [
+                    'model' => $users,
+                    'show_url' => route('admin.users.show', $users->id),
+                    'edit_url' => route('admin.users.edit', $users->id),
+                    'delete_url' => route('admin.users.destroy', $users->id),
+                ]);
+            })
+            ->rawColumns(['user', 'action'])
+            ->make(true);
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -31,3 +20,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// method resource berfungi membuatkan otomatis method yang ada pada controller (inde,show,create,store,edit,update,destroy)
+Route::resource('/admin/users', 'UsersController', ['as' => 'admin']);
+
+Route::get('/api/datatable/users', 'UsersController@dataTable')->name('api.datatable.users');
+
