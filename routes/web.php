881b157b9c3cb73b1392253a1e39ea1dfaecd145
zoe606 <rive99erlangga@gmail.com>
2018-12-28 22:28:14 +0700

    frontend layout

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/blog', function () {
+    return view('blog');
+});
+
 // Auth::routes();
 
 // Authentication Routes...
