23ce18421c34ec89ff7a27dcf1a5eebd2bd69bcf
zoe606 <rive99erlangga@gmail.com>
2018-12-28 15:55:18 +0700

    settings

---
+++ app/Http/Controllers/SettingsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Setting;
+use Illuminate\Http\Request;
+
+class SettingsController extends Controller
+{
+    public function index() {
+
+        $setting = Setting::first();
+        return view('admin.settings.index', compact('setting'));
+    }
+
+    public function store(Request $request) {
+        $this->validate($request,[
+            'title' => 'required',
+            'tagline' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required',
+            'address' => 'required',
+        ]);
+
+        Setting::updateOrCreate([
+            'id' => 1
+        ], $request->all());
+
+        return redirect()->route('admin.settings.index');
+    }
+}
